5926b8508bc0472ce8e42d97da24e8a3ab830091
AltonC2 <143027301+AltonC2@users.noreply.github.com>
2024-04-22 22:02:14 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -3,8 +3,22 @@ public class RandomLetterChooser extends RandomStringChooser
 	
 	public RandomLetterChooser(String str)
 	{ 
+		private String [] values;
+		private int valuesRemaining;
+		public RandomLetterChooser(String[] vals){
+values = new String[vals.length];
+			for ( int i = 0; i < values.length; i++){
+values[i] = vals[i];
+				valuesRemaining = values.length;
+			}
+		}
 		/* to be implemented in part (b) */
-		
+		public String getNext()
+			{
+if (valuesRemaining==0)
+	return  "NONE";
+			int index = (int) (Math.random()*valuesRemaining)
+		}
 	}
 	
 	
